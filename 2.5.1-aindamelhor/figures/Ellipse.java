8f4f6a42f45d2f761aba35a4b61a8cbca6b7e604
curiousmind-squared <64873152+curiousmind-squared@users.noreply.github.com>
2022-03-08 21:57:42 -0300

    Add files via upload

---
+++ 2.5.1-aindamelhor/figures/Ellipse.java
@@ -0,0 +1,40 @@
+package figures;
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Ellipse2D.Double;
+
+public class Ellipse {
+    private int x, y;
+    private int w, h;
+    private int[] cc; // c == contorno
+    private int[] cf; // f == fundo
+
+
+    public Ellipse (int x, int y, int w, int h, int[] cc, int[] cf) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.cc = cc;
+        this.cf = cf;
+    }
+
+    public void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        int rc = this.cc[0]; int gc = this.cc[1]; int bc = this.cc[2];
+        int rf = this.cf[0]; int gf = this.cf[1]; int bf = this.cf[2];
+
+
+
+        Graphics2D g2d = (Graphics2D) g;
+        g.setColor(new Color(rf, gf, bf));
+        g2d.fill(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+        g.setColor(new Color(rc, gc, bc));
+        g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+    }
+}
